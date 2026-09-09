cdf8af195845ff2ad2b3c07eaa266c4caa3d8d03
alexisgtierrz <alexisgutierrez1072@gmail.com>
2026-09-09 15:51:37 -0300

    Inicialización del proyecto Spring Boot

---
+++ src/main/java/com/cooperativa/coop_servicios_backend/CoopServiciosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cooperativa.coop_servicios_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopServiciosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopServiciosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooperativa/coop_servicios_backend/CoopServiciosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperativa.coop_servicios_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopServiciosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
